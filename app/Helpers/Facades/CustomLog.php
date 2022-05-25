51614fda9a8aae68bdf54e3720db8aad4fb5454e
namph <namph.paraline@gmail.com>
2022-05-25 16:51:45 +0700

    fix custom log

--- app/Helpers/Facades/CustomLog.php
+++ app/Helpers/Facades/CustomLog.php
@@ -1,11 +1,22 @@
 <?php
 namespace App\Helpers\Facades;
 
+use App\Helpers\ChannelWriter;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * Class ChannelLog
  * @package App\Helpers\Facades
+ */
+/**
+ * @method static void logInfo(string $message, array $context = [])
+ * @method static void logError(string $message, array $context = [])
+ * @method static void logDebug(string $message, array $context = [])
+ * @method static void logWarning(string $message, array $context = [])
+ * @method static void logApi(string $message, array $context = [])
+ * @method static void logBatch(string $message, array $context = [])
+ *
+ * @see ChannelWriter
  */
 class CustomLog extends Facade
 {
