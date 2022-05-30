ee42456d17d13545f1fbc9df74881ed4a63d8e6e
namph <namph.paraline@gmail.com>
2022-05-30 10:14:31 +0700

    demo api using passport

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('login', 'api\UserController@login');
+Route::post('register', 'api\UserController@register');
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('details', 'api\UserController@details');
+    Route::get('logout', 'api\UserController@logout');
 });
